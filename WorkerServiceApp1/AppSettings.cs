ba3a8e4f5d868cbdb76137cb1b2f8b1a97b07a4e
Sumitbisht0014 <sumitbisht0014@gmail.com>
2021-03-15 10:14:39 +0530

    Initial Checkin

---
+++ WorkerServiceApp1/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerServiceApp1
+{
+    public static class AppSettings
+    {
+        public static IConfiguration Configuration { get; set; }
+        public static string ConnectionString { get; set; }
+    }
+}
